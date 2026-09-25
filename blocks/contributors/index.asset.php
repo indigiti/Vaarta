55a22ed7ea908d11c566ada25c8feeffe88e25dd
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 00:34:39 +0530

    feat(contributors): add blocks/contributors/index.asset.php

---
+++ blocks/contributors/index.asset.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	'dependencies' => array(
+		'wp-blocks',
+		'wp-block-editor',
+		'wp-components',
+		'wp-core-data',
+		'wp-data',
+		'wp-element',
+		'wp-i18n',
+	),
+	'version' => '0.1.0',
+);
