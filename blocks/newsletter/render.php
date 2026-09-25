383da3026b79dd844211f652b0c24c07c25e6951
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 00:14:14 +0530

    fix(newsletter): harden unconfigured form state and field IDs

--- blocks/newsletter/render.php
+++ blocks/newsletter/render.php
@@ -15,6 +15,7 @@ $button_label     = isset( $attributes['buttonLabel'] ) ? sanitize_text_field( $
 $form_action      = isset( $attributes['formAction'] ) ? esc_url( $attributes['formAction'] ) : '';
 $email_field_name = isset( $attributes['emailFieldName'] ) ? sanitize_key( $attributes['emailFieldName'] ) : 'EMAIL';
 $style_variant    = isset( $attributes['styleVariant'] ) ? sanitize_key( $attributes['styleVariant'] ) : 'light';
+$field_id         = wp_unique_id( 'vaarta-newsletter-email-' );
 
 $allowed_styles = array( 'light', 'dark', 'minimal' );
 if ( ! in_array( $style_variant, $allowed_styles, true ) ) {
@@ -38,21 +39,28 @@ $wrapper_attributes = get_block_wrapper_attributes(
 		<?php endif; ?>
 	</div>
 
-	<form class="vaarta-newsletter__form" action="<?php echo esc_url( $form_action ); ?>" method="post">
-		<label class="screen-reader-text" for="vaarta-newsletter-email-<?php echo esc_attr( wp_unique_id() ); ?>">
-			<?php esc_html_e( 'Email address', 'vaarta' ); ?>
-		</label>
-		<input
-			id="vaarta-newsletter-email-<?php echo esc_attr( wp_unique_id() ); ?>"
-			class="vaarta-newsletter__email"
-			type="email"
-			name="<?php echo esc_attr( $email_field_name ); ?>"
-			placeholder="<?php echo esc_attr__( 'Email address', 'vaarta' ); ?>"
-			autocomplete="email"
-			required
-		/>
-		<button class="vaarta-newsletter__button" type="submit">
-			<?php echo esc_html( $button_label ); ?>
-		</button>
-	</form>
+	<?php if ( $form_action ) : ?>
+		<form class="vaarta-newsletter__form" action="<?php echo esc_url( $form_action ); ?>" method="post">
+			<label class="screen-reader-text" for="<?php echo esc_attr( $field_id ); ?>">
+				<?php esc_html_e( 'Email address', 'vaarta' ); ?>
+			</label>
+			<input
+				id="<?php echo esc_attr( $field_id ); ?>"
+				class="vaarta-newsletter__email"
+				type="email"
+				name="<?php echo esc_attr( $email_field_name ); ?>"
+				placeholder="<?php echo esc_attr__( 'Email address', 'vaarta' ); ?>"
+				autocomplete="email"
+				required
+			/>
+			<button class="vaarta-newsletter__button" type="submit">
+				<?php echo esc_html( $button_label ); ?>
+			</button>
+		</form>
+	<?php else : ?>
+		<div class="vaarta-newsletter__form vaarta-newsletter__form--disabled" aria-label="<?php echo esc_attr__( 'Newsletter form preview', 'vaarta' ); ?>">
+			<input class="vaarta-newsletter__email" type="email" placeholder="<?php echo esc_attr__( 'Email address', 'vaarta' ); ?>" disabled />
+			<button class="vaarta-newsletter__button" type="button" disabled><?php echo esc_html( $button_label ); ?></button>
+		</div>
+	<?php endif; ?>
 </section>
