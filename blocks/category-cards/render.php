3f16b6a337fdf7eabcd57f9bba1a54103e849a17
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 01:31:11 +0530

    feat(pixel): render latest category featured images

--- blocks/category-cards/render.php
+++ blocks/category-cards/render.php
@@ -14,6 +14,7 @@ $category_slugs   = isset( $attributes['categorySlugs'] ) && is_array( $attribut
 	? array_values( array_filter( array_map( 'sanitize_title', $attributes['categorySlugs'] ) ) )
 	: array();
 $max_categories   = isset( $attributes['maxCategories'] ) ? absint( $attributes['maxCategories'] ) : 5;
+$show_image       = ! array_key_exists( 'showImage', $attributes ) || ! empty( $attributes['showImage'] );
 $show_count       = ! array_key_exists( 'showCount', $attributes ) || ! empty( $attributes['showCount'] );
 $show_description = ! empty( $attributes['showDescription'] );
 $layout           = isset( $attributes['layout'] ) ? sanitize_key( $attributes['layout'] ) : 'grid';
@@ -66,9 +67,37 @@ $wrapper_attributes = get_block_wrapper_attributes(
 			}
 			?>
 			<article class="vaarta-category-card">
-				<div class="vaarta-category-card__mark" aria-hidden="true">
-					<?php echo esc_html( strtoupper( substr( $term->name, 0, 1 ) ) ); ?>
-				</div>
+				<?php
+				$thumbnail_id = 0;
+
+				if ( $show_image ) {
+					$latest_ids = get_posts(
+						array(
+							'post_type'           => 'post',
+							'post_status'         => 'publish',
+							'posts_per_page'      => 1,
+							'category'            => $term->term_id,
+							'fields'              => 'ids',
+							'ignore_sticky_posts' => true,
+							'no_found_rows'       => true,
+						)
+					);
+
+					if ( $latest_ids ) {
+						$thumbnail_id = get_post_thumbnail_id( $latest_ids[0] );
+					}
+				}
+				?>
+
+				<?php if ( $thumbnail_id ) : ?>
+					<a class="vaarta-category-card__media" href="<?php echo esc_url( $url ); ?>" aria-hidden="true" tabindex="-1">
+						<?php echo wp_get_attachment_image( $thumbnail_id, 'medium_large', false, array( 'loading' => 'lazy', 'decoding' => 'async' ) ); ?>
+					</a>
+				<?php else : ?>
+					<div class="vaarta-category-card__mark" aria-hidden="true">
+						<?php echo esc_html( strtoupper( substr( $term->name, 0, 1 ) ) ); ?>
+					</div>
+				<?php endif; ?>
 
 				<div class="vaarta-category-card__body">
 					<h3 class="vaarta-category-card__title">
