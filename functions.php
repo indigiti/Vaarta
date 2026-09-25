912103bbc5eb98991b8b5d089b6e2c0ef8eb0868
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 00:40:27 +0530

    feat(search): load instant-search REST endpoint

--- functions.php
+++ functions.php
@@ -16,6 +16,7 @@ require_once get_theme_file_path( 'inc/query-variations.php' );
 require_once get_theme_file_path( 'inc/content.php' );
 require_once get_theme_file_path( 'inc/views.php' );
 require_once get_theme_file_path( 'inc/contributors.php' );
+require_once get_theme_file_path( 'inc/search.php' );
 require_once get_theme_file_path( 'inc/block-styles.php' );
 require_once get_theme_file_path( 'inc/dark-mode.php' );
 require_once get_theme_file_path( 'inc/blocks.php' );
