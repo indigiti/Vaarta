1fd77ab2dc9f54e0bda3e71a7a52f9e12d3a7f5d
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 00:04:37 +0530

    feat(editorial): add inc/blocks.php

---
+++ inc/blocks.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Vaarta custom Gutenberg blocks.
+ *
+ * @package Vaarta
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Register theme-owned blocks.
+ */
+function vaarta_register_blocks(): void {
+	$blocks = array(
+		'editorial-grid',
+	);
+
+	foreach ( $blocks as $block ) {
+		$path = get_theme_file_path( 'blocks/' . $block );
+
+		if ( file_exists( $path . '/block.json' ) ) {
+			register_block_type( $path );
+		}
+	}
+}
+add_action( 'init', 'vaarta_register_blocks' );
