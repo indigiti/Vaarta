6fba304a6133bb511c784963f126f8a243dfa332
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-25 23:58:20 +0530

    refactor(theme): add functions.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Vaarta bootstrap.
+ *
+ * @package Vaarta
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+require_once get_theme_file_path( 'inc/setup.php' );
+require_once get_theme_file_path( 'inc/assets.php' );
+require_once get_theme_file_path( 'inc/patterns.php' );
+require_once get_theme_file_path( 'inc/query-variations.php' );
