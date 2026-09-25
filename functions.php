b8e29c5f629c04739e706f6bbff71a88d1007080
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 00:07:08 +0530

    feat(styles): load Core block style registrations

--- functions.php
+++ functions.php
@@ -14,4 +14,5 @@ require_once get_theme_file_path( 'inc/assets.php' );
 require_once get_theme_file_path( 'inc/patterns.php' );
 require_once get_theme_file_path( 'inc/query-variations.php' );
 require_once get_theme_file_path( 'inc/content.php' );
+require_once get_theme_file_path( 'inc/block-styles.php' );
 require_once get_theme_file_path( 'inc/blocks.php' );
