09788ef688981085e1b80e63577c18d5b0453a3e
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 01:47:13 +0530

    feat(ads): add patterns/ad-inline.php

---
+++ patterns/ad-inline.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Ad — Inline
+ * Slug: vaarta/ad-inline
+ * Categories: vaarta, vaarta-promo
+ * Inserter: yes
+ */
+?>
+<!-- wp:vaarta/ad-slot {"slotName":"content-inline","minHeight":250,"showLabel":true,"collapseWhenEmpty":false,"align":"wide"} /-->
