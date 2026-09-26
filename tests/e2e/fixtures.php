2d0dd5518e5bb2fe3be4e0c656d79edd7eeb58f8
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 12:54:38 +0530

    Tests: seed nested fullscreen navigation

--- tests/e2e/fixtures.php
+++ tests/e2e/fixtures.php
@@ -29,37 +29,54 @@ function vaarta_e2e_menu( $name ) {
 	return (int) $menu_id;
 }
 
-$primary_id = vaarta_e2e_menu( 'Primary Navigation' );
+/**
+ * Add a custom-link item and fail loudly when WordPress rejects the fixture.
+ *
+ * @param int    $menu_id   Menu ID.
+ * @param string $title     Item title.
+ * @param string $url       Link URL.
+ * @param int    $parent_id Optional parent menu-item ID.
+ * @return int
+ */
+function vaarta_e2e_menu_item( $menu_id, $title, $url, $parent_id = 0 ) {
+	$item_id = wp_update_nav_menu_item(
+		$menu_id,
+		0,
+		array(
+			'menu-item-title'     => $title,
+			'menu-item-url'       => $url,
+			'menu-item-status'    => 'publish',
+			'menu-item-type'      => 'custom',
+			'menu-item-object'    => 'custom',
+			'menu-item-parent-id' => $parent_id,
+		)
+	);
+
+	if ( is_wp_error( $item_id ) ) {
+		throw new RuntimeException( $item_id->get_error_message() );
+	}
+
+	return (int) $item_id;
+}
+
+$primary_id    = vaarta_e2e_menu( 'Primary Navigation' );
 $fullscreen_id = vaarta_e2e_menu( 'Fullscreen Navigation' );
 
 if ( ! wp_get_nav_menu_items( $primary_id ) ) {
-	wp_update_nav_menu_item(
-		$primary_id,
-		0,
-		array(
-			'menu-item-title'  => 'Home',
-			'menu-item-url'    => home_url( '/' ),
-			'menu-item-status' => 'publish',
-		)
-	);
+	vaarta_e2e_menu_item( $primary_id, 'Home', home_url( '/' ) );
 }
 
 if ( ! wp_get_nav_menu_items( $fullscreen_id ) ) {
-	wp_update_nav_menu_item(
-		$fullscreen_id,
-		0,
-		array(
-			'menu-item-title'  => 'Stories',
-			'menu-item-url'    => home_url( '/' ),
-			'menu-item-status' => 'publish',
-		)
-	);
+	$stories_id = vaarta_e2e_menu_item( $fullscreen_id, 'Stories', '#' );
+	$news_id    = vaarta_e2e_menu_item( $fullscreen_id, 'News', '#', $stories_id );
+	vaarta_e2e_menu_item( $fullscreen_id, 'World', home_url( '/?s=world' ), $news_id );
+	vaarta_e2e_menu_item( $fullscreen_id, 'Culture', home_url( '/?s=culture' ), $stories_id );
 }
 
-$locations = (array) get_theme_mod( 'nav_menu_locations', array() );
-$locations['primary'] = $primary_id;
-$locations['mobile'] = $primary_id;
-$locations['fullscreen'] = $fullscreen_id;
+$locations               = (array) get_theme_mod( 'nav_menu_locations', array() );
+$locations['primary']     = $primary_id;
+$locations['mobile']      = $primary_id;
+$locations['fullscreen']  = $fullscreen_id;
 set_theme_mod( 'nav_menu_locations', $locations );
 
 set_theme_mod( 'header_navigation_menu', true );
