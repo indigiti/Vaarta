ad2f7a7e5902834f0da031b0f43a5fa18e425a97
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 11:45:48 +0530

    Blocks: declare Story Carousel dependencies

---
+++ blocks/story-carousel/index.asset.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	'dependencies' => array(
+		'wp-block-editor',
+		'wp-blocks',
+		'wp-components',
+		'wp-core-data',
+		'wp-data',
+		'wp-element',
+		'wp-i18n',
+		'wp-server-side-render',
+	),
+	'version'      => '2.0.0-alpha.1',
+);
