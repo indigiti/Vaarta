dae9d9175b5e96d2c7133dad01c95d971367a334
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 11:32:46 +0530

    Blocks: declare Story Meta editor dependencies

---
+++ blocks/story-meta/index.asset.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'dependencies' => array(
+		'wp-block-editor',
+		'wp-blocks',
+		'wp-components',
+		'wp-element',
+		'wp-i18n',
+		'wp-server-side-render',
+	),
+	'version'      => '2.0.0-alpha.1',
+);
