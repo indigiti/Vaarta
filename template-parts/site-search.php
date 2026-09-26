1f02de75a73614f56df25e46c49241c06a6a6a22
indigiti <34906944+indigiti@users.noreply.github.com>
2026-09-26 12:18:08 +0530

    Chrome: make site search accessible

--- template-parts/site-search.php
+++ template-parts/site-search.php
@@ -1,17 +1,19 @@
 <?php
 /**
- * The template part for displaying site section.
+ * The template part for displaying site search.
  *
- * @package Caards
+ * @package Vaarta
  */
 
 $scheme = csco_color_scheme(
 	get_theme_mod( 'color_header_background', '#ffffff' ),
 	get_theme_mod( 'color_header_background_dark', '#1b1c1f' )
 );
+
+$search_placeholder = get_theme_mod( 'misc_search_placeholder', esc_html__( 'Enter keyword', 'caards' ) );
 ?>
 
-<div class="cs-search cs-search-<?php echo esc_attr( csco_get_header_search_type() ); ?>" <?php echo wp_kses( $scheme, 'csco' ); ?>>
+<div id="vaarta-site-search" class="cs-search cs-search-<?php echo esc_attr( csco_get_header_search_type() ); ?>" aria-hidden="true" <?php echo wp_kses( $scheme, 'csco' ); ?>>
 
 	<?php if ( 'two' === csco_get_header_search_type() ) { ?>
 		<div class="cs-container">
@@ -20,19 +22,19 @@ $scheme = csco_color_scheme(
 	<div class="cs-search__wrapper">
 		<form role="search" method="get" class="cs-search__nav-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 			<div class="cs-search__group">
-				<input data-swpparentel=".cs-header .cs-search-live-result" required class="cs-search__input" data-swplive="true" type="search" value="<?php the_search_query(); ?>" name="s" placeholder="<?php echo esc_attr( get_theme_mod( 'misc_search_placeholder', esc_html__( 'Enter keyword', 'caards' ) ) ); ?>">
+				<input data-swpparentel=".cs-header .cs-search-live-result" required class="cs-search__input" data-swplive="true" type="search" value="<?php the_search_query(); ?>" name="s" placeholder="<?php echo esc_attr( $search_placeholder ); ?>" aria-label="<?php echo esc_attr( $search_placeholder ); ?>">
 
-				<button class="cs-search__submit" type="submit">
-					<i class="cs-icon cs-icon-search"></i>
+				<button class="cs-search__submit" type="submit" aria-label="<?php esc_attr_e( 'Search', 'caards' ); ?>">
+					<i class="cs-icon cs-icon-search" aria-hidden="true"></i>
 				</button>
 
-				<button class="cs-search__close">
-					<span></span>
+				<button class="cs-search__close" type="button" aria-label="<?php esc_attr_e( 'Close search', 'caards' ); ?>">
+					<span aria-hidden="true"></span>
 				</button>
 			</div>
 		</form>
 
-		<div class="cs-search-live-container">
+		<div class="cs-search-live-container" aria-live="polite" aria-atomic="false">
 			<div class="cs-search-live-result"></div>
 		</div>
 
